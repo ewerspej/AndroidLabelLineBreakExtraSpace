3f3a63c9c1d028e6d4b79e68f1b038fb693d5b51
Julian Ewers-Peters <ewerspej@tcd.ie>
2024-06-05 16:20:15 +0200

    initial commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AndroidLabelLineBreakExtraSpace
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
